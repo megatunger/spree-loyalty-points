80059b597e33a0bdc8c999b067684298c6ac8ae4
andrea longhi <andrea@spaghetticode.it>
2018-04-24 09:45:36 +0200

    fix translation key

--- app/overrides/add_loyalty_points_to_order_checkout_page.rb
+++ app/overrides/add_loyalty_points_to_order_checkout_page.rb
@@ -6,10 +6,10 @@ Deface::Override.new(virtual_path: 'spree/shared/_order_details',
       <tr id='loyalty-points-row'>
         <% if @order.loyalty_points_awarded? %>
           <td></td>
-          <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> <%= t' spree_loyalty_points.have_been_credited_to_your_account' %></b></td>
+          <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> <%= t' spree.have_been_credited_to_your_account' %></b></td>
         <% else %>
           <td></td>
-          <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> <%= t 'spree_loyalty_points.will_be_credited_to_your_account_soon' %></b></td>
+          <td colspan='4' class='total'><span><%= @order.loyalty_points_for(@order.item_total) %></span> <b><%= Spree.t(:loyalty_points) %> <%= t 'spree.will_be_credited_to_your_account_soon' %></b></td>
         <% end %>
       </tr>
     </tfoot>
