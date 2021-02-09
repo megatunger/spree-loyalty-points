0a0af1feaada7d74b2757713bffed2d803c698e6
Tien Ta <taminhtien1993@gmail.com>
2021-02-09 10:48:39 +0700

    Fix bad query

--- app/decorators/models/spree_loyalty_points/spree/order_decorator.rb
+++ app/decorators/models/spree_loyalty_points/spree/order_decorator.rb
@@ -14,7 +14,7 @@ module SpreeLoyaltyPoints
         base.has_many :loyalty_points_debit_transactions, as: :source
 
         base.scope :loyalty_points_not_awarded, -> { includes(:loyalty_points_credit_transactions).where(spree_loyalty_points_transactions: { source_id: nil } ) }
-        base.scope :with_hours_since_payment, ->(num) { where('`spree_orders`.`paid_at` < ? ', num.hours.ago) }
+        base.scope :with_hours_since_payment, ->(num) { where('spree_orders.paid_at < ? ', num.hours.ago) }
         base.scope :with_uncredited_loyalty_points, ->(num) { with_hours_since_payment(num).loyalty_points_not_awarded }
 
         fsm = base.state_machines[:state]
